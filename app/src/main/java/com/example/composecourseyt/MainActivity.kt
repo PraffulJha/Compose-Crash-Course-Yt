23e4ac1ad67218055915d6865bf8460f4e697448
Prafful <praffuljha1234@gmail.com>
2024-03-21 12:15:21 +0530

    constraintlayout in compose

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -16,32 +16,52 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
-            LazyColumn(
-//                modifier = Modifier.verticalScroll(scrollState)
-            ) {
-                itemsIndexed(listOf("This","is","Jetpack","Compose")) { index, string ->
-                   Text(
-                       text = "Item: $string", // lazy load - load on scrolling item
-                       fontSize = 24.sp,
-                       fontWeight = FontWeight.Bold,
-                       textAlign = TextAlign.Center,
-                       modifier = Modifier
-                           .fillMaxWidth()
-                           .padding(vertical = 24.dp)
-                   )
-               }
+            val constraintsSet = ConstraintSet{
+                val greenBox = createRefFor("greenbox")
+                val blueBox = createRefFor("blueBox")
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.matchParent
+                    height = Dimension.value(100.dp)
+                }
+                constrain(blueBox) {
+                    top.linkTo(greenBox.bottom)
+                    start.linkTo(parent.start)
+                    width = Dimension.matchParent
+                    height = Dimension.value(100.dp)
+                }
+            }
+            ConstraintLayout(constraintsSet, modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .layoutId("greenbox")
+                ){
+                    Text(text = "This is Green Box")
+                }
+                Box(
+                    modifier = Modifier
+                        .background(Color.Blue)
+                        .layoutId("blueBox")
+                ){
+                    Text(text = "This is Blue Box")
+                }
             }
         }
     }
